7c0cabf5cabdc115e3e8502dd787810422f5a14d
enihsgnir <enihsgnir@gmail.com>
2024-08-14 14:31:25 +0900

    test: add format test cases

--- test/utils/format_test.dart
+++ test/utils/format_test.dart
@@ -2,6 +2,19 @@ import 'package:dears/utils/formats.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test("number format test", () {
+    expect(number.format(123), "123");
+    expect(number.format(1234), "1,234");
+    expect(number.format(1234567), "1,234,567");
+    expect(number.format(null), "-");
+  });
+
+  test("rating format test", () {
+    expect(rating.format(0.1), "0.1");
+    expect(rating.format(3), "3.0");
+    expect(rating.format(null), "-");
+  });
+
   test("search format test", () {
     expect(search.format("hello"), "hello");
     expect(search.format("  hello  "), "hello");
