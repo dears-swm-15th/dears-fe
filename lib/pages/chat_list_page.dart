0433ce402db4c940f4160cc49a2439c3d68ca88b
enihsgnir <enihsgnir@gmail.com>
2024-08-17 22:02:34 +0900

    refactor: apply reusable widget

--- lib/pages/chat_list_page.dart
+++ lib/pages/chat_list_page.dart
@@ -1,6 +1,6 @@
 import 'package:dears/providers/chat_list_provider.dart';
-import 'package:dears/utils/theme.dart';
 import 'package:dears/widgets/chat_list_tile.dart';
+import 'package:dears/widgets/list_status_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -11,22 +11,9 @@ class ChatListPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final chatList = ref.watch(chatListProvider);
 
-    final emptyWidget = Center(
-      child: Column(
-        children: [
-          const Spacer(),
-          const Text(
-            "대화 중인 웨딩플래너가 없습니다",
-            style: titleMedium,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            "마음에 드는 웨딩플래너와 대화해보세요",
-            style: bodySmall.copyWith(color: gray600),
-          ),
-          const Spacer(flex: 2),
-        ],
-      ),
+    const emptyWidget = EmptyListWidget(
+      title: "대화 중인 웨딩플래너가 없습니다",
+      subtitle: "마음에 드는 웨딩플래너와 대화해보세요",
     );
 
     final child = chatList.when(
@@ -35,28 +22,20 @@ class ChatListPage extends ConsumerWidget {
           return emptyWidget;
         }
 
-        return ListView.separated(
-          physics: const ClampingScrollPhysics(),
-          itemCount: data.length,
-          separatorBuilder: (context, index) => const SizedBox(height: 10),
-          itemBuilder: (context, index) {
-            final chat = data[index];
-            return ChatListTile(chat);
-          },
-        );
-      },
-      error: (error, stackTrace) => emptyWidget,
-      loading: () {
-        return const Center(
-          child: Column(
-            children: [
-              Spacer(),
-              CircularProgressIndicator(),
-              Spacer(flex: 2),
-            ],
+        return Expanded(
+          child: ListView.separated(
+            physics: const ClampingScrollPhysics(),
+            itemCount: data.length,
+            separatorBuilder: (context, index) => const SizedBox(height: 10),
+            itemBuilder: (context, index) {
+              final chat = data[index];
+              return ChatListTile(chat);
+            },
           ),
         );
       },
+      error: (error, stackTrace) => emptyWidget,
+      loading: () => const LoadingListWidget(),
     );
 
     return Scaffold(
@@ -66,7 +45,7 @@ class ChatListPage extends ConsumerWidget {
       body: Column(
         children: [
           const SizedBox(height: 24),
-          Expanded(child: child),
+          child,
         ],
       ),
     );
