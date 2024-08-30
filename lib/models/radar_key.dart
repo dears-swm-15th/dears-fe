1cba946e0a4ceff97bec6ecf2c168bed331bc8b7
enihsgnir <enihsgnir@gmail.com>
2024-08-30 11:41:31 +0900

    feat: refine radar chart

--- lib/models/radar_key.dart
+++ lib/models/radar_key.dart
@@ -4,9 +4,9 @@ import 'package:json_annotation/json_annotation.dart';
 enum RadarKey {
   communication("의사소통"),
   budgetCompliance("예산 준수"),
-  personalCustomization("개인 맞춤"),
-  priceRationality("가격 합리성"),
   scheduleCompliance("일정 준수"),
+  priceRationality("가격 합리성"),
+  personalCustomization("개인 맞춤"),
   ;
 
   final String name;
