6e30c869e0892fc51a6c000aca7a235bbf8f3ca8
enihsgnir <enihsgnir@gmail.com>
2024-09-09 15:16:27 +0900

    feat: init firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:dears/firebase_options.dart';
 import 'package:dears/providers/router_provider.dart';
 import 'package:dears/providers/stomp_provider.dart';
 import 'package:dears/utils/provider_observer.dart';
 import 'package:dears/utils/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       observers: [MainObserver()],
