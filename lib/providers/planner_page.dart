02b568f7a071b9e36a8fea190739549e64affe65
Jinwoo Choi <cjw1234577@gmail.com>
2024-09-09 21:11:04 +0900

    add: planner page

---
+++ lib/providers/planner_page.dart
@@ -0,0 +1,25 @@
+import 'package:dears/providers/user_info_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class PlannerPage extends ConsumerWidget {
+  const PlannerPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Planner Page'),
+      ),
+      body: Center(
+        child: FilledButton(
+          onPressed: () {
+            ref.read(userInfoProvider.notifier).logout();
+          },
+          child: const Text("Logout"),
+        ),
+      ),
+    );
+  }
+}
