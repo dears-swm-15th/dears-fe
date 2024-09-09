d8cf3884373096f35e62372bbbdd5b521b825af6
enihsgnir <enihsgnir@gmail.com>
2024-09-09 13:36:59 +0900

    docs: add documentation comments

--- lib/providers/role_interceptor_provider.dart
+++ lib/providers/role_interceptor_provider.dart
@@ -4,6 +4,9 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'role_interceptor_provider.g.dart';
 
+/// A provider that returns an interceptor that replaces the path with the role.
+/// It gets the role from the [UserInfo] and prepends the role to the path
+/// if the path starts with `/{role}`.
 @riverpod
 Future<Interceptor> roleInterceptor(RoleInterceptorRef ref) async {
   final role = await ref.watch(
