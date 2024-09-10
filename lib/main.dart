30b8ff5c942c38af745da6776afd2b0f36b1acc7
enihsgnir <enihsgnir@gmail.com>
2024-09-10 12:34:14 +0900

    feat: integrate firebase crashlytics for error reporting

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:dears/providers/stomp_provider.dart';
 import 'package:dears/utils/provider_observer.dart';
 import 'package:dears/utils/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -13,6 +15,16 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  }
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(
     const ProviderScope(
