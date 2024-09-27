dce9a495ed69557f4250e05b163793ec5fe2c286
enihsgnir <enihsgnir@gmail.com>
2024-09-27 13:47:17 +0900

    refactor: reusable sign-in button widget

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,8 +1,7 @@
 import 'package:dears/providers/auth_state_provider.dart';
-import 'package:dears/utils/logger.dart';
 import 'package:dears/utils/theme.dart';
+import 'package:dears/widgets/sign_in_button.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SignInPage extends ConsumerWidget {
@@ -47,85 +46,31 @@ class SignInPage extends ConsumerWidget {
                 ),
               ),
               const Spacer(flex: 2),
-              GestureDetector(
-                onTap: () async {
-                  try {
-                    await ref
-                        .read(authStateProvider.notifier)
-                        .signIn(const GoogleOAuth2Provider());
-
-                    if (!context.mounted) return;
-                    context.go("/");
-                  } catch (e, s) {
-                    logger.d(
-                      "failed to sign in with Google",
-                      error: e,
-                      stackTrace: s,
-                    );
-                  }
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: gray600),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        "assets/g-logo.png",
-                        width: 18,
-                      ),
-                      const Spacer(),
-                      const Text("Google로 로그인"),
-                      const Spacer(),
-                    ],
-                  ),
+              SignInButton(
+                provider: const GoogleOAuth2Provider(),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: gray600),
+                  borderRadius: BorderRadius.circular(8),
                 ),
+                prefix: Image.asset(
+                  "assets/g-logo.png",
+                  width: 18,
+                ),
+                title: const Text("Google로 로그인"),
               ),
               const SizedBox(height: 10),
-              GestureDetector(
-                onTap: () async {
-                  try {
-                    await ref
-                        .read(authStateProvider.notifier)
-                        .signIn(const KakaoOAuth2Provider());
-
-                    if (!context.mounted) return;
-                    context.go("/");
-                  } catch (e, s) {
-                    logger.d(
-                      "failed to sign in with Kakao",
-                      error: e,
-                      stackTrace: s,
-                    );
-                  }
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFEE500),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        "assets/kakao_symbol.png",
-                        width: 18,
-                      ),
-                      const Spacer(),
-                      const Text("카카오로 로그인"),
-                      const Spacer(),
-                    ],
-                  ),
+              SignInButton(
+                provider: const KakaoOAuth2Provider(),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFEE500),
+                  borderRadius: BorderRadius.circular(8),
                 ),
+                prefix: Image.asset(
+                  "assets/kakao_symbol.png",
+                  width: 18,
+                ),
+                title: const Text("카카오로 로그인"),
               ),
               const Spacer(),
             ],
