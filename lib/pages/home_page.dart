1db799905ed73274d192d2e44642f78f20941c92
enihsgnir <enihsgnir@gmail.com>
2024-10-09 14:57:05 +0900

    feat: add home banner

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:dears/utils/icons.dart';
 import 'package:dears/utils/theme.dart';
 import 'package:dears/widgets/custom_app_bar.dart';
+import 'package:dears/widgets/home_banner.dart';
 import 'package:dears/widgets/home_navigation_bar.dart';
 import 'package:dears/widgets/home_top_viewed_list.dart';
 import 'package:flutter/material.dart';
@@ -30,8 +31,10 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: ListView(
+        physics: const ClampingScrollPhysics(),
         padding: EdgeInsets.zero,
         children: const [
+          HomeBanner(),
           SizedBox(height: 20),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 16),
