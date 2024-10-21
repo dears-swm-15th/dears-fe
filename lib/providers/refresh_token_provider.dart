205943c9b47abda009ea26cb82c1030addf18d1c
enihsgnir <enihsgnir@gmail.com>
2024-10-21 14:36:14 +0900

    feat: keep providers stored in secure storage alive

--- lib/providers/refresh_token_provider.dart
+++ lib/providers/refresh_token_provider.dart
@@ -1,31 +1,15 @@
 import 'package:dears/providers/secure_storage_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'refresh_token_provider.g.dart';
 
-const String _key = "refresh_token";
-
-@riverpod
-class RefreshToken extends _$RefreshToken {
+@Riverpod(keepAlive: true)
+class RefreshToken extends _$RefreshToken with SecureStorageProviderMixin {
+  @protected
   @override
-  Future<String?> build() async {
-    final storage = ref.watch(storageProvider);
-    return storage.read(key: _key);
-  }
+  final key = "refresh_token";
 
-  Future<String?> _save(String? value) async {
-    final storage = ref.read(storageProvider);
-    await storage.write(key: _key, value: value);
-    return value;
-  }
-
-  Future<void> setValue(String value) async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() => _save(value));
-  }
-
-  Future<void> clear() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() => _save(null));
-  }
+  @override
+  Future<String?> build();
 }
--- lib/providers/uuid_provider.dart
+++ lib/providers/uuid_provider.dart
@@ -1,31 +1,15 @@
 import 'package:dears/providers/secure_storage_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'uuid_provider.g.dart';
 
-const String _key = "uuid";
-
-@riverpod
-class Uuid extends _$Uuid {
+@Riverpod(keepAlive: true)
+class Uuid extends _$Uuid with SecureStorageProviderMixin {
+  @protected
   @override
-  Future<String?> build() async {
-    final storage = ref.watch(storageProvider);
-    return storage.read(key: _key);
-  }
+  final key = "uuid";
 
-  Future<String?> _save(String? value) async {
-    final storage = ref.read(storageProvider);
-    await storage.write(key: _key, value: value);
-    return value;
-  }
-
-  Future<void> setValue(String value) async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() => _save(value));
-  }
-
-  Future<void> clear() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() => _save(null));
-  }
+  @override
+  Future<String?> build();
 }
